95f708b109cd07f54d1512e597668c7aeebde4c1
Nikos Zapantis <86017435+NikosZapantis@users.noreply.github.com>
2024-05-28 23:40:14 +0300

    Database Setup {Added}

---
+++ db_setup.js
@@ -0,0 +1,20 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('books.sqlite');
+
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS regBooks (
+        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Author VARCHAR(25) NOT NULL,
+        Title VARCHAR(40) NOT NULL,
+        Genre VARCHAR(20) NOT NULL,
+        Price REAL NOT NULL
+    )`);
+
+    db.run(`INSERT INTO regBooks (Title, Author, Genre, Price) VALUES
+    ('Pirates of the Caribbean', 'Johnny Depp', 'Action and Adventure', 12.99),
+    ('The Pirate Hunter', 'Nikos Zapantis', 'Mystery', 15.99),
+    ('Pirate Latitudes', 'Nikos Zapantis', 'Horror', 39.99)
+    `);
+});
+
+db.close();
