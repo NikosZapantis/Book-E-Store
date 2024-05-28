00a4a2959c924c9f91dddf6c7df752e448e98695
Nikos Zapantis <86017435+NikosZapantis@users.noreply.github.com>
2024-05-28 23:40:33 +0300

    Endpoints for RESTful API Post & Get {Added}

---
+++ serverEndpointsAPI.js
@@ -0,0 +1,21 @@
+//? Get function for search feature
+app.get('/regBooks/:keyword', (req, res) => {
+    const keyword = req.params.keyword;
+    db.all('SELECT * FROM books WHERE title LIKE ?', [`%${keyword}%`], (err, rows) => {
+        if(err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json(rows);
+    });
+});
+
+//? Post function for add feature
+app.post('/books', (req, res) => {
+    const { Author, Title, Genre, Price } = req.body;
+    db.run('INSERT INTO books (author, title, genre, price) VALUES (?, ?, ?, ?)', [Author, Title, Genre, Price], function(err) {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      res.json({ message: 'Book added successfully', Id: this.lastID });
+    });
+});
