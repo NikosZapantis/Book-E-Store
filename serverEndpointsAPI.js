5a9ee53fca2efce3ceb43c2368c2e3690ea57ec9
Nikos Zapantis <86017435+NikosZapantis@users.noreply.github.com>
2024-05-29 12:59:16 +0300

    Correct Routes {Updated}

--- serverEndpointsAPI.js
+++ serverEndpointsAPI.js
@@ -1,7 +1,7 @@
 //? Get function for search feature
 app.get('/regBooks/:keyword', (req, res) => {
     const keyword = req.params.keyword;
-    db.all('SELECT * FROM books WHERE title LIKE ?', [`%${keyword}%`], (err, rows) => {
+    db.all('SELECT * FROM regBooks WHERE Title LIKE ?', [`%${keyword}%`], (err, rows) => {
         if(err) {
             return res.status(500).json({ error: err.message });
         }
@@ -10,7 +10,7 @@ app.get('/regBooks/:keyword', (req, res) => {
 });
 
 //? Post function for add feature
-app.post('/books', (req, res) => {
+app.post('/regBooks', (req, res) => {
     const { Author, Title, Genre, Price } = req.body;
     db.run('INSERT INTO books (author, title, genre, price) VALUES (?, ?, ?, ?)', [Author, Title, Genre, Price], function(err) {
       if (err) {
